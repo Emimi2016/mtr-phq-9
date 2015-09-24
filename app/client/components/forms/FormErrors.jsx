534cf22af70765f30db2bb3964c688cc52dbe124
Damir <damir@withpulp.com>
2015-09-24 16:03:42 -0400

    created formInputs and formErrors

---
+++ app/client/components/forms/FormErrors.jsx
@@ -0,0 +1,18 @@
+App.FormErrors = React.createClass({
+    propTypes: {
+        errors: React.PropTypes.object
+    },
+    render() {
+        if (this.props.errors) {
+            return (
+                <div className="errors alerts module">
+                    {
+                        _.values(this.props.errors).map((errorMessage) => {
+                            return <p key={errorMessage} className="negative alert">{errorMessage}</p>;
+                        })
+                    }
+                </div>
+            )
+        }
+    }
+});
