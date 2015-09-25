e02def4da7e470a5020bc4cb1a15c6f402939c44
Damir <damir@withpulp.com>
2015-09-25 13:25:44 -0400

    working on multi step form wizard - question 1 done

---
+++ app/client/components/forms/wizard/FormWizard.jsx
@@ -0,0 +1,85 @@
+var fieldValues = {
+    question1: null,
+    question2: null,
+    question3: null,
+    question4: null,
+    question5: null,
+    question6: null,
+    question7: null,
+    question8: null,
+    question9: null
+};
+
+App.FormWizard = React.createClass({
+    mixins: [],
+    PropTypes: {},
+
+    getInitialState: function () {
+        return {
+            step: 1
+        }
+    },
+
+    saveValues: function (field_value) {
+        return function () {
+            fieldValues = objectAssign({}, fieldValues, field_value)
+        }.bind(this)()
+    },
+
+    nextStep: function () {
+        this.setState({
+            step: this.state.step + 1
+        })
+    },
+
+    previousStep: function () {
+        this.setState({
+            step: this.state.step - 1
+        })
+    },
+
+    submitRegistration: function () {
+        // Handle submitting the user data, upon
+        // success return this.nextStop(). If it fails,
+        // show the user the error but don't advance
+
+        this.nextStep()
+    },
+
+    showStep: function () {
+        switch (this.state.step) {
+            case 1:
+                return <App.QuestionOne fieldValues={fieldValues}
+                                        nextStep={this.nextStep}
+                                        previousStep={this.previousStep}
+                                        saveValues={this.saveValues}/>
+            case 2:
+                return <App.QuestionTwo fieldValues={fieldValues}
+                                        nextStep={this.nextStep}
+                                        previousStep={this.previousStep}
+                                        saveValues={this.saveValues}/>
+            case 3:
+                return <App.QuestionThree fieldValues={fieldValues}
+                                          previousStep={this.previousStep}
+                                          submitRegistration={this.submitRegistration}/>
+            case 4:
+                return <App.WizardPositive fieldValues={fieldValues}/>
+        }
+    },
+
+    render: function () {
+        var style = {
+            width: (this.state.step / 4 * 100) + '%'
+        }
+
+        return (
+            <div className="form wizard module">
+                {this.showStep()}
+                <div className="progress module">
+                    <span className="progress-step">Step {this.state.step}</span>
+                    <progress className="progress" style={style}></progress>
+                </div>
+            </div>
+        )
+    }
+});
---
+++ app/client/components/forms/wizard/QuestionOne.jsx
@@ -0,0 +1,38 @@
+App.QuestionOne = React.createClass({
+    renderOptions: function(type, name, value, index) {
+        var isChecked = function() {
+            if (type == 'radio') return value == this.props.fieldValues[name]
+
+            return false
+        }.bind(this)
+
+        return (
+            <label key={index}>
+                <input type={type} name={name} value={value} defaultChecked={isChecked()} /> {value}
+            </label>
+        )
+    },
+
+    render: function () {
+        return (
+            <div>
+                <h2>Little interest or pleasure in doing things?</h2>
+                {['Not at all', 'Several days', 'More than half the days', 'Nearly every day'].map(this.renderOptions.bind(this, 'radio', 'question1'))}
+                <button className="button primary" onClick={this.nextStep}>Save &amp; Continue</button>
+            </div>
+        )
+    },
+
+    nextStep: function (e) {
+        e.preventDefault()
+
+        // Get values via query selector
+        var question1 = document.querySelector('input[name="question1"]:checked');
+        var data = {
+            question1: question1
+        }
+
+        this.props.saveValues(data)
+        this.props.nextStep()
+    }
+})
