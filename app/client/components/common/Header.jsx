3686ecfd8291e7e82771957f8fe976d2c8ebba9d
Damir <damir@withpulp.com>
2015-09-28 09:06:20 -0400

    refactored header component

--- app/client/components/common/Header.jsx
+++ app/client/components/common/Header.jsx
@@ -1,19 +1,17 @@
 App.Header = React.createClass({
+    renderStatement() {
+        return (
+            <p className="animated fadeInDown statement">
+                Over the last two weeks, how often have you been bothered by any of the following problems?
+            </p>
+        )
+    },
+
     render() {
-        if (FlowRouter.getRouteName() === 'Root') {
-            return (
-                <header className="header">
-                    <p className="animated fadeInDown statement">
-                        Over the last two weeks, how often have you been bothered by any of the following problems?
-                    </p>
-                </header>
-            )
-        } else {
-            return (
-                <header className="header">
-                    <App.Quotes />
-                </header>
-            )
-        }
+        return (
+            <header className="header">
+                {(FlowRouter.getRouteName() === 'Root') ? this.renderStatement() : <App.Quotes />}
+            </header>
+        )
     }
 });
